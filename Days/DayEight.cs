6d338eb33bd497c74749984ad34492067dffd1f3
Michael Shepherd <michael.shepherd.contact@gmail.com>
2023-12-06 12:46:28 +0200

    Changed day 8 inoput file name

--- Days/DayEight.cs
+++ Days/DayEight.cs
@@ -11,13 +11,13 @@ public static class DayEight
 
     public static long HandleStepOne()
     {
-        var input = InputReader.Get(".\\input\\day_seven_input.txt");
+        var input = InputReader.Get(".\\input\\eight.txt");
         return -1;
     }
 
     public static long HandleStepTwo()
     {
-        var input = InputReader.Get(".\\input\\day_seven_input.txt");
+        var input = InputReader.Get(".\\input\\eight.txt");
         return -1;
     }
 }
